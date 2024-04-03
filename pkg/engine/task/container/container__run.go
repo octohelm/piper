d5e883a69be4adb71f1ca86d62ee51d84a74b16e
Morlay <morlay.null@gmail.com>
2024-04-03 09:49:24 +0800

    fix: container.#Run normalize shell

--- pkg/engine/task/container/container__run.go
+++ pkg/engine/task/container/container__run.go
@@ -2,6 +2,7 @@ package container
 
 import (
 	"context"
+	"fmt"
 	"github.com/octohelm/piper/pkg/cueflow"
 	"github.com/octohelm/piper/pkg/engine/task"
 	"github.com/octohelm/piper/pkg/engine/task/client"
@@ -23,9 +24,8 @@ type Run struct {
 	User    string                           `json:"user,omitempty" default:"root:root"`
 	Always  bool                             `json:"always,omitempty"`
 
-	Shell string `json:"shell,omitempty" default:"sh"`
-
-	Run client.StringOrSlice `json:"run"`
+	Shell string               `json:"shell,omitempty" default:"sh"`
+	Run   client.StringOrSlice `json:"run"`
 
 	Output Container `json:"-" output:"output"`
 }
@@ -40,8 +40,13 @@ func (x *Run) Do(ctx context.Context) error {
 	exec.User = x.User
 	exec.Always = x.Always
 
+	if strings.HasPrefix(x.Shell, "/") {
+		exec.Entrypoint = []string{x.Shell}
+	} else {
+		exec.Entrypoint = []string{fmt.Sprintf("/bin/%s", x.Shell)}
+	}
 	exec.Args = []string{
-		x.Shell, "-c", strings.Join(x.Run, "\n"),
+		"-c", strings.Join(x.Run, "\n"),
 	}
 
 	if err := exec.Do(ctx); err != nil {
